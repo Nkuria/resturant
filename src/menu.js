39bd7e773393e2c1dc20b9637ddcebc97f366090
Nick Kuria <nkuria01@gmail.com>
2021-04-11 22:27:09 +0300

    Corrected errors

--- src/menu.js
+++ src/menu.js
@@ -1,133 +1,150 @@
-import {mainFood as mainFood, salads as salads, drinks as drinks, deserts as deserts} from './menuObject';
-function menuPage (body) {
-// const body = document.getElementById('content')
-const menuContainer = document.createElement('div');
-menuContainer.classList.add('menu-container');
-body.appendChild(menuContainer);
+import {
+  mainFood as mainFood,
+  salads as salads,
+  drinks as drinks,
+  deserts as deserts,
+} from "./menuObject";
 
-const menuHeader = document.createElement('h3');
-const menuHeaderText = document.createTextNode('Discover');
-menuHeader.appendChild(menuHeaderText);
-menuHeader.classList.add('menu-header');
-menuContainer.appendChild(menuHeader);
+function menuPage(body) {
+  // const body = document.getElementById('content')
+  const menuContainer = document.createElement("div");
+  menuContainer.classList.add("menu-container");
+  body.appendChild(menuContainer);
 
-const menuHeader2 = document.createElement('h2');
-const menuHeader2Text = document.createTextNode('Our Main Food Menu');
-menuHeader2.appendChild(menuHeader2Text);
-menuHeader2.classList.add('menu-header2');
-menuContainer.appendChild(menuHeader2);
+  const menuHeader = document.createElement("h3");
+  const menuHeaderText = document.createTextNode("Discover");
+  menuHeader.appendChild(menuHeaderText);
+  menuHeader.classList.add("menu-header");
+  menuContainer.appendChild(menuHeader);
 
-function foodLoops() {
-    const foodContainer = document.createElement('div');
-    foodContainer.classList.add('food-container');
+  const menuHeader2 = document.createElement("h2");
+  const menuHeader2Text = document.createTextNode("Our Main Food Menu");
+  menuHeader2.appendChild(menuHeader2Text);
+  menuHeader2.classList.add("menu-header2");
+  menuContainer.appendChild(menuHeader2);
+
+  function foodLoops() {
+    const foodContainer = document.createElement("div");
+    foodContainer.classList.add("food-container");
     menuContainer.appendChild(foodContainer);
 
-    const leftMenu = document.createElement('div');
-    leftMenu.classList.add('left-menu');
+    const leftMenu = document.createElement("div");
+    leftMenu.classList.add("left-menu");
     foodContainer.appendChild(leftMenu);
 
-    function MainFoodPart () {
-        const MainFoodHeader = document.createElement('h3');
-        MainFoodHeader.classList.add('food-header');
-        const MainFoodHeaderText = document.createTextNode("Main Course");
-        MainFoodHeader.appendChild(MainFoodHeaderText);
-        leftMenu.appendChild(MainFoodHeader);
+    function MainFoodPart() {
+      const MainFoodHeader = document.createElement("h3");
+      MainFoodHeader.classList.add("food-header");
+      const MainFoodHeaderText = document.createTextNode("Main Course");
+      MainFoodHeader.appendChild(MainFoodHeaderText);
+      leftMenu.appendChild(MainFoodHeader);
 
-        for(let i = 0; i < salads.length; i++){
-            const saladName = document.createElement('p');
-            const saladNameText = document.createTextNode(mainFood[i].name + "..................................................." + mainFood[i].price);
-            saladName.classList.add('salad-name');
-            saladName.appendChild(saladNameText);
-            leftMenu.appendChild(saladName);
+      for (let i = 0; i < salads.length; i++) {
+        const saladName = document.createElement("p");
+        const saladNameText = document.createTextNode(
+          mainFood[i].name +
+            "..................................................." +
+            mainFood[i].price
+        );
+        saladName.classList.add("salad-name");
+        saladName.appendChild(saladNameText);
+        leftMenu.appendChild(saladName);
 
-            const saladIng = document.createElement('p');
-            const saladIngText = document.createTextNode(mainFood[i].ingredients);
-            saladIng.classList.add('salad-ing');
-            saladIng.appendChild(saladIngText);
-            leftMenu.appendChild(saladIng);
-
-        }
-       
-    }
-    function desertsPart () {
-        const MainFoodHeader = document.createElement('h3');
-        MainFoodHeader.classList.add('food-header');
-        const MainFoodHeaderText = document.createTextNode("Deserts");
-        MainFoodHeader.appendChild(MainFoodHeaderText);
-        leftMenu.appendChild(MainFoodHeader);
-
-        for(let i = 0; i < salads.length; i++){
-            const saladName = document.createElement('p');
-            const saladNameText = document.createTextNode(salads[i].name + "..................................................." + salads[i].price);
-            saladName.classList.add('salad-name');
-            saladName.appendChild(saladNameText);
-            leftMenu.appendChild(saladName);
-
-            const saladIng = document.createElement('p');
-            const saladIngText = document.createTextNode(salads[i].ingredients);
-            saladIng.classList.add('salad-ing');
-            saladIng.appendChild(saladIngText);
-            leftMenu.appendChild(saladIng);
-        }
-       
+        const saladIng = document.createElement("p");
+        const saladIngText = document.createTextNode(mainFood[i].ingredients);
+        saladIng.classList.add("salad-ing");
+        saladIng.appendChild(saladIngText);
+        leftMenu.appendChild(saladIng);
+      }
     }
 
-    const rightMenu = document.createElement('div');
-    leftMenu.classList.add('right-menu');
+    function desertsPart() {
+      const MainFoodHeader = document.createElement("h3");
+      MainFoodHeader.classList.add("food-header");
+      const MainFoodHeaderText = document.createTextNode("Deserts");
+      MainFoodHeader.appendChild(MainFoodHeaderText);
+      leftMenu.appendChild(MainFoodHeader);
+
+      for (let i = 0; i < salads.length; i++) {
+        const saladName = document.createElement("p");
+        const saladNameText = document.createTextNode(
+          salads[i].name +
+            "..................................................." +
+            salads[i].price
+        );
+        saladName.classList.add("salad-name");
+        saladName.appendChild(saladNameText);
+        leftMenu.appendChild(saladName);
+
+        const saladIng = document.createElement("p");
+        const saladIngText = document.createTextNode(salads[i].ingredients);
+        saladIng.classList.add("salad-ing");
+        saladIng.appendChild(saladIngText);
+        leftMenu.appendChild(saladIng);
+      }
+    }
+
+    const rightMenu = document.createElement("div");
+    leftMenu.classList.add("right-menu");
     foodContainer.appendChild(rightMenu);
 
-    function drinksPart () {
-        const MainFoodHeader = document.createElement('h3');
-        MainFoodHeader.classList.add('food-header');
-        const MainFoodHeaderText = document.createTextNode("Drinks");
-        MainFoodHeader.appendChild(MainFoodHeaderText);
-        rightMenu.appendChild(MainFoodHeader);
+    function drinksPart() {
+      const MainFoodHeader = document.createElement("h3");
+      MainFoodHeader.classList.add("food-header");
+      const MainFoodHeaderText = document.createTextNode("Drinks");
+      MainFoodHeader.appendChild(MainFoodHeaderText);
+      rightMenu.appendChild(MainFoodHeader);
 
-        for(let i = 0; i < salads.length; i++){
-            const saladName = document.createElement('p');
-            const saladNameText = document.createTextNode(salads[i].name + "..................................................." + salads[i].price);
-            saladName.classList.add('salad-name');
-            saladName.appendChild(saladNameText);
-            rightMenu.appendChild(saladName);
+      for (let i = 0; i < salads.length; i++) {
+        const saladName = document.createElement("p");
+        const saladNameText = document.createTextNode(
+          salads[i].name +
+            "..................................................." +
+            salads[i].price
+        );
+        saladName.classList.add("salad-name");
+        saladName.appendChild(saladNameText);
+        rightMenu.appendChild(saladName);
 
-            const saladIng = document.createElement('p');
-            const saladIngText = document.createTextNode(salads[i].ingredients);
-            saladIng.classList.add('salad-ing');
-            saladIng.appendChild(saladIngText);
-            rightMenu.appendChild(saladIng);
-        }
-       
+        const saladIng = document.createElement("p");
+        const saladIngText = document.createTextNode(salads[i].ingredients);
+        saladIng.classList.add("salad-ing");
+        saladIng.appendChild(saladIngText);
+        rightMenu.appendChild(saladIng);
+      }
     }
 
-    function saladsPart () {
-        const MainFoodHeader = document.createElement('h3');
-        MainFoodHeader.classList.add('food-header');
-        const MainFoodHeaderText = document.createTextNode("Salads");
-        MainFoodHeader.appendChild(MainFoodHeaderText);
-        rightMenu.appendChild(MainFoodHeader);
+    function saladsPart() {
+      const MainFoodHeader = document.createElement("h3");
+      MainFoodHeader.classList.add("food-header");
+      const MainFoodHeaderText = document.createTextNode("Salads");
+      MainFoodHeader.appendChild(MainFoodHeaderText);
+      rightMenu.appendChild(MainFoodHeader);
 
-        for(let i = 0; i < salads.length; i++){
-            const saladName = document.createElement('p');
-            const saladNameText = document.createTextNode(salads[i].name + "..................................................." + salads[i].price);
-            saladName.classList.add('salad-name');
-            saladName.appendChild(saladNameText);
-            rightMenu.appendChild(saladName);
+      for (let i = 0; i < salads.length; i++) {
+        const saladName = document.createElement("p");
+        const saladNameText = document.createTextNode(
+          salads[i].name +
+            "..................................................." +
+            salads[i].price
+        );
+        saladName.classList.add("salad-name");
+        saladName.appendChild(saladNameText);
+        rightMenu.appendChild(saladName);
 
-            const saladIng = document.createElement('p');
-            const saladIngText = document.createTextNode(salads[i].ingredients);
-            saladIng.classList.add('salad-ing');
-            saladIng.appendChild(saladIngText);
-            rightMenu.appendChild(saladIng);
-        }
-       
+        const saladIng = document.createElement("p");
+        const saladIngText = document.createTextNode(salads[i].ingredients);
+        saladIng.classList.add("salad-ing");
+        saladIng.appendChild(saladIngText);
+        rightMenu.appendChild(saladIng);
+      }
     }
 
-
-MainFoodPart();
-desertsPart();
-drinksPart();
-saladsPart();
+    MainFoodPart();
+    desertsPart();
+    drinksPart();
+    saladsPart();
+  }
+  foodLoops();
 }
-foodLoops();
-}
-export {menuPage};
+export { menuPage };
